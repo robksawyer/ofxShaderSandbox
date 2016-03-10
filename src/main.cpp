d45fdb55c24dc14454cea4a18c3506a821acb4b6
Rob Sawyer <rob.sawyer@razorfish.com>
2016-03-10 02:52:08 -0800

    Added ofxFPSControls

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ int main( ){
 	settings.height = 768;
 	ofCreateWindow(settings);       // <-------- setup the GL context
 
-
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
